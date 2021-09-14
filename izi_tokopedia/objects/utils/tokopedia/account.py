416dea8fc09ee22e57a78f1c07b30c8c995c9d6d
Gusti Tammam <gustitammam@gmail.com>
2021-09-14 22:46:58 +0700

    [ADD][izi_marketplace,izi_tokopedia] Update Tokopedia Auth

--- izi_tokopedia/objects/utils/tokopedia/account.py
+++ izi_tokopedia/objects/utils/tokopedia/account.py
@@ -3,24 +3,33 @@
 import requests
 from base64 import b64encode
 
-from .endpoint import ENDPOINTS
+from .endpoint import TokopediaEndpoint
 
 
 class TokopediaAccount(object):
 
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id, client_secret, **kwargs):
         self.client_id = client_id
         self.client_secret = client_secret
+        self.fs_id = kwargs.get('fs_id', None)
+        self.access_token = kwargs.get('access_token', None)
+        self.expired_date = kwargs.get('expired_date', None)
+        self.token_type = kwargs.get('token_type', None)
+        self.endpoints = TokopediaEndpoint(self, host="accounts")
 
-    def get_auth(self):
-        auth = b64encode('{}:{}'.format(self.client_id, self.client_secret).encode()).decode()
-        return 'Basic %s' % auth
+    def get_auth(self, token=False):
+        if token:
+            auth = 'Basic %s' % b64encode('{}:{}'.format(self.client_id, self.client_secret).encode()).decode()
+        else:
+            auth = '%s %s' % (self.token_type, self.access_token)
+        return auth
 
     def authenticate(self):
         headers = {
-            'Authorization': self.get_auth(),
-            'Content-Length': '0',
-            'User-Agent': 'PostmanRuntime/7.17.1'
+            'Authorization': self.get_auth(token=True),
         }
-        response = requests.post(ENDPOINTS.get('auth'), headers=headers)
+        prepared_request = self.endpoints.build_request('token', **{
+            'headers': headers
+        })
+        response = requests.request(**prepared_request)
         return response.json()
