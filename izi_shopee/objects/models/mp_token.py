37db3df5576aa2c06620caf5c290f6d223b99e82
rheynoapria <rheynoapria11@gmail.com>
2021-09-15 11:49:23 +0700

    [UPDATE] authenticate shopee

--- izi_shopee/objects/models/mp_token.py
+++ izi_shopee/objects/models/mp_token.py
@@ -10,15 +10,18 @@ from odoo import api, fields, models
 class MarketplaceToken(models.Model):
     _inherit = 'mp.token'
 
+    sp_refresh_token = fields.Char(string="Shopee Refresh Token", readonly=True)
+
     @api.model
     def shopee_create_token(self, mp_account, raw_token):
         mp_token_obj = self.env['mp.token']
 
-        expired_date = datetime.now() + relativedelta(seconds=raw_token.get('expires_in'))
+        expired_date = datetime.now() + relativedelta(seconds=raw_token.get('expire_in'))
         values = {
             'name': raw_token.get('access_token'),
             'expired_date': fields.Datetime.to_string(expired_date),
             'mp_account_id': mp_account.id,
+            'sp_refresh_token': raw_token.get('refresh_token'),
             'raw': json.dumps(raw_token, indent=4)
         }
         mp_token_obj.create(values)
--- izi_shopee/objects/utils/shopee/account.py
+++ izi_shopee/objects/utils/shopee/account.py
@@ -13,74 +13,54 @@ _logger = logging.getLogger(__name__)
 
 class ShopeeAccount(object):
 
-    def __init__(self, partner_id, partner_key):
+    def __init__(self, partner_id, partner_key, base_url=False, mp_id=False, shop_id=False, code=False, refresh_token=False):
         self.partner_id = partner_id
         self.partner_key = partner_key
+        self.base_url = base_url
+        self.mp_id = mp_id
+        self.shop_id = shop_id
+        self.code = code
+        self.refresh_token = refresh_token
 
 
     def get_redirect_url(self):
-        if self.ensure_one() and isinstance(self.id, int):
-            base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-            return '%s/api/user/auth/shopee/%s' % (base_url, self.id)
-
+        return '%s/api/user/auth/shopee/%s' % (self.base_url, self.mp_id)
+    
     def get_auth_url_v2(self):
         timeest = int(time.time())
         base_string = '%s%s%s' % (self.partner_id, ENDPOINTS_PATH.get('auth'), timeest)
         sign = hmac.new(self.partner_key.encode(), base_string.encode(), hashlib.sha256).hexdigest()
-        if self.ensure_one() and isinstance(self.id, int):
-            return '%(host)s%(path)s?partner_id=%(partner_id)s&redirect=%(redirect)s&timestamp=%(timestamp)s&sign=%(sign)s' % {
-                'host': BASE_ENDPOINTS.get('sp_base_url'),
-                'path': ENDPOINTS_PATH.get('auth'),
-                'partner_id': self.partner_id,
-                'redirect': urllib.parse.quote(self.get_redirect_url()),
-                'timestamp': timeest,
-                'sign': sign
-            }
+        return '%(host)s%(path)s?partner_id=%(partner_id)s&redirect=%(redirect)s&timestamp=%(timestamp)s&sign=%(sign)s' % {
+            'host': BASE_ENDPOINTS.get('sp_base_url'),
+            'path': ENDPOINTS_PATH.get('auth'),
+            'partner_id': self.partner_id,
+            'redirect': urllib.parse.quote(self.get_redirect_url()),
+            'timestamp': timeest,
+            'sign': sign
+        }
     
-    def get_token(self, **kwargs):
-        mp_token_obj = self.env['mp.token']
-
-        code = kwargs.get('code')
-        shop_id = kwargs.get('shop_id')
-        refresh_token = kwargs.get('refresh_token')
-        last_code_id = kwargs.get('last_code_id')
-
-        if self.ensure_one() and shop_id: 
+    def get_token(self):
+        if self.shop_id:
             timeest = int(time.time())
-            base_string = '%s%s%s%s' % (self.partner_id, ENDPOINTS_PATH.get('token_renew') if refresh_token else ENDPOINTS_PATH.get('token_get'), timeest, shop_id)
+            base_string = '%s%s%s%s' % (self.partner_id, ENDPOINTS_PATH.get('token_renew') if self.refresh_token else ENDPOINTS_PATH.get('token_get'), timeest, self.shop_id)
             sign = hmac.new(self.partner_key.encode(), base_string.encode(), hashlib.sha256).hexdigest()
             resp = requests.post('%(url)s%(path)s?partner_id=%(partner_id)s&shop_id=%(shop_id)s&timestamp=%(timestamp)s&sign=%(sign)s' % {
                 'url': BASE_ENDPOINTS.get('sp_base_url'),
-                'path': ENDPOINTS_PATH.get('token_renew') if refresh_token else ENDPOINTS_PATH.get('token_get'),
-                'partner_id': self.partner_id,
-                'shop_id': int(shop_id),
+                'path': ENDPOINTS_PATH.get('token_renew') if self.refresh_token else ENDPOINTS_PATH.get('token_get'),
+                'partner_id': int(self.partner_id),
+                'shop_id': int(self.shop_id),
                 'timestamp': timeest,
                 'sign': sign
             }, json={
-                **({'refresh_token': refresh_token} if refresh_token else {'code': code}),
-                'shop_id': int(shop_id),
-                'partner_id': self.partner_id
+                **({'refresh_token': self.refresh_token} if self.refresh_token else {'code': self.code}),
+                'shop_id': int(self.shop_id),
+                'partner_id': int(self.partner_id)
             }, headers={
                 'Content-Type': 'application/json'
             })
-            
+        
             _logger.info('\n%s' % (json.dumps(resp.json(), indent=2)))
-            if resp.json().get('access_token') and resp.json().get('refresh_token') and  resp.json().get('expire_in'):
-                mp_token_obj.create_token(self, resp.json)
-                
-            #     code_id = mp_token_obj.create({
-            #         'mp_id': self.id,
-            #         'code': code,
-            #         'shop_id': shop_id,
-            #         'access_token': r.json().get('access_token'),
-            #         'refresh_token': r.json().get('refresh_token'),
-            #         'expires_in': r.json().get('expire_in')
-            #     })
-            #     if last_code_id:
-            #         last_code_id.unlink()
-            # else:
-            #     code_id = False
             
-            # return code_id
+            return resp.json()
 
     
