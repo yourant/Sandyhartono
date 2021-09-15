48ea5c8c904ab2280734de07be2cb3553f293c76
Hanifurrohman <hanifurrohmanm@gmail.com>
2021-09-15 16:03:41 +0700

    [ADD]auth blibli

---
+++ izi_blibli/objects/utils/blibli/endpoint.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 IZI PT Solusi Usaha Mudah
+
+BASE_ENDPOINTS = {
+    'sp_base_url': 'https://partner.shopeemobile.com'
+}
+
+ENDPOINTS_PATH = {
+    'auth': '/api/v2/shop/auth_partner',
+    'token_renew': '/api/v2/auth/access_token/get',
+    'token_get': '/api/v2/auth/token/get'
+}
