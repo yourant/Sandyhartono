4d758dd01d1c0353d8343e3a4a506f96e677f85b
Gusti Tammam <gustitammam@gmail.com>
2021-09-17 14:06:49 +0700

    [ADD][izi_tokopedia] Add Get Product API

--- izi_tokopedia/objects/utils/tokopedia/__init__.py
+++ izi_tokopedia/objects/utils/tokopedia/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2021 IZI PT Solusi Usaha Mudah
 
+from . import api
 from . import tools
 from . import exception
 from . import endpoint
 from . import account
+from . import shop
+from . import product
---
+++ izi_tokopedia/objects/utils/tokopedia/product.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 IZI PT Solusi Usaha Mudah
+
+from .api import TokopediaAPI
+
+
+class TokopediaProduct(TokopediaAPI):
+
+    def __init__(self, tp_account):
+        super(TokopediaProduct, self).__init__(tp_account)
+        self.product_data = []
+
+    def get_product_info(self, shop_id, limit=0, per_page=50):
+        params = {
+            'shop_id': shop_id
+        }
+        unlimited = not limit
+        if unlimited:
+            page = 1
+            while unlimited:
+                params.update({
+                    'page': page,
+                    'per_page': per_page
+                })
+                prepared_request = self.endpoints.build_request('product_info', **{
+                    'params': params
+                })
+                tp_data = self.process_response(self.request(**prepared_request))
+                if tp_data:
+                    self.product_data.extend(tp_data)
+                    page += 1
+                else:
+                    unlimited = False
+        else:
+            pagination_pages = self.pagination_get_pages(limit=limit, per_page=per_page)
+            for pagination_page in pagination_pages:
+                params.update({
+                    'page': pagination_page[0],
+                    'per_page': pagination_page[1]
+                })
+                prepared_request = self.endpoints.build_request('product_info', **{
+                    'params': params
+                })
+                tp_data = self.process_response(self.request(**prepared_request))
+                if tp_data:
+                    self.product_data.extend(tp_data)
+
+        return self.product_data
+
+
+
