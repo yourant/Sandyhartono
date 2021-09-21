e350b3c6d8f21dc2e1a6bb3263fa50f4d9228d1c
Gusti Tammam <gustitammam@gmail.com>
2021-09-21 07:11:58 +0700

    [IMP][izi_marketplace] Make marketplace field stored compute field.

--- izi_marketplace/objects/models/mp_base.py
+++ izi_marketplace/objects/models/mp_base.py
@@ -17,7 +17,7 @@ class MarketplaceBase(models.AbstractModel):
     _rec_mp_field_mapping = {}
 
     mp_account_id = fields.Many2one(comodel_name="mp.account", string="Marketplace Account", required=True)
-    marketplace = fields.Selection(related="mp_account_id.marketplace", readonly=True)
+    marketplace = fields.Char(string="Marketplace", compute="_compute_marketplace", store=True)
     raw = fields.Text(string="Raw Data", readonly=True, required=True, default="{}")
     signature = fields.Char(string="Signature", readonly=True, required=True, default="",
                             help="MD5 hash of the marketplace data.")
@@ -44,6 +44,12 @@ class MarketplaceBase(models.AbstractModel):
     @api.model
     def _get_rec_mp_field_mapping(self, marketplace):
         return self._rec_mp_field_mapping.get(marketplace, None)
+
+    @api.multi
+    @api.depends('mp_account_id.marketplace')
+    def _compute_marketplace(self):
+        for rec in self:
+            rec.marketplace = rec.mp_account_id.marketplace
 
     @api.multi
     def _compute_mp_external_id(self):
