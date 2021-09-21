3e549286af2114558cc16f2ba43d6886dde8a411
rheynoapria <rheynoapria11@gmail.com>
2021-09-21 12:56:02 +0700

    [IMP][izi_shopee,izi_marketplace] create product

--- izi_shopee/objects/models/mp_product.py
+++ izi_shopee/objects/models/mp_product.py
@@ -13,6 +13,12 @@ class MarketplaceProduct(models.Model):
     @classmethod
     def _build_model_attributes(cls, pool):
 
+        def _handle_price_info(data):
+            if data:
+                return data[0].get('original_price')
+            else:
+                return None
+
         cls._rec_mp_external_id = dict(cls._rec_mp_external_id, **{
             'shopee': {
                 'mp_external_id': 'sp_product_id'
@@ -21,16 +27,16 @@ class MarketplaceProduct(models.Model):
 
         cls._rec_mp_field_mapping = dict(cls._rec_mp_field_mapping, **{
             'shopee': {
-                'name': ('item_name', None),
-                'description_sale': ('description', None),
-                'default_code': ('item_sku', lambda r: r if r else False),
-                'list_price': ('price_info/original_price', None),
-                'weight': ('weight', lambda r: float(r)),
-                'length': ('dimension/package_length', lambda r: float(r)),
-                'width': ('dimension/package_width', lambda r: float(r)),
-                'height': ('dimension/package_height', lambda r: float(r)),
-                'sp_product_id': ('item_id', None),
-                'sp_item_status': ('item_status', None)
+                'name': ('item_list/item_name', None),
+                'description_sale': ('item_list/description', None),
+                'default_code': ('item_list/item_sku', lambda r: r if r else False),
+                'list_price': ('item_list/price_info', _handle_price_info),
+                'weight': ('item_list/weight', lambda r: float(r)),
+                'length': ('item_list/dimension/package_length', lambda r: float(r)),
+                'width': ('item_list/dimension/package_width', lambda r: float(r)),
+                'height': ('item_list/dimension/package_height', lambda r: float(r)),
+                'sp_product_id': ('item_list/item_id', None),
+                'sp_item_status': ('item_list/item_status', None)
             }
         })
         super(MarketplaceProduct, cls)._build_model_attributes(pool)
@@ -39,5 +45,5 @@ class MarketplaceProduct(models.Model):
     def shopee_get_sanitizers(self, mp_field_mapping):
         default_sanitizer = self.get_default_sanitizer(mp_field_mapping, root_path='response')
         return {
-            'product_detail': default_sanitizer
+            'product_info': default_sanitizer
         }
