43bb284bd448b4881212d2a5f65e5f8af4a0befa
rheynoapria <rheynoapria11@gmail.com>
2021-10-04 13:32:53 +0700

    [IMP][izi_marketplace,izi_shopee] order wizard

--- izi_marketplace/objects/wizard/wiz_mp_order.py
+++ izi_marketplace/objects/wizard/wiz_mp_order.py
@@ -9,6 +9,7 @@ class MPOrderWizard(models.TransientModel):
 
     _name = 'wiz.mp.get.order'
 
+    mp_account_id = fields.Many2one(comodel_name='mp.account', string='MP Account')
     order_start_date = fields.Datetime(string='Get Start Time Order', required=True,
                                        default=lambda self: fields.Datetime.to_string(
                                            datetime.now() - timedelta(days=3))
@@ -17,10 +18,10 @@ class MPOrderWizard(models.TransientModel):
                                      fields.Datetime.to_string(datetime.now()))
 
     def get_order(self):
-        mp_account_obj = self.env['mp.account']
         self.ensure_one()
         if self.order_end_date < self.order_start_date:
             raise UserError('End Datetime field must be higger from Start Datetime ..')
         else:
-            if hasattr(mp_account_obj, '%s_get_orders' % self.env.context.get('marketplace')):
-                getattr(mp_account_obj, '%s_get_orders' % self.env.context.get('marketplace'))()
+            if hasattr(self.mp_account_id, '%s_get_orders' % self.env.context.get('marketplace')):
+                getattr(self.mp_account_id, '%s_get_orders' % self.env.context.get('marketplace'))(
+                    date_from=self.order_start_date, date_to=self.order_end_date)
