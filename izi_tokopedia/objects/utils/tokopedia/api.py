cc4e990aa776db5310334748e9c696c20c5ccb82
Gusti Tammam <gustitammam@gmail.com>
2021-10-05 09:04:34 +0700

    [ADD][izi_mp,izi_tp,izi_sp] Initialize Order API

--- izi_tokopedia/objects/utils/tokopedia/api.py
+++ izi_tokopedia/objects/utils/tokopedia/api.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2021 IZI PT Solusi Usaha Mudah
 import logging
+from datetime import datetime
 
+import pytz
 import requests
 
-from .tools import validate_response, sanitize_response, pagination_get_pages
+from .tools import validate_response, sanitize_response, pagination_get_pages, pagination_date_range
 from .endpoint import TokopediaEndpoint
 
 
@@ -13,6 +15,7 @@ class TokopediaAPI(object):
     def __init__(self, tp_account, api_version="v1", **kwargs):
         self.tp_account = tp_account
         self.api_version = api_version
+        self.api_tz = pytz.timezone('Asia/Jakarta')
         self.endpoints = TokopediaEndpoint(tp_account, api_version=api_version)
         self.build_request = self.endpoints.build_request
         self.request = requests.request
@@ -24,8 +27,19 @@ class TokopediaAPI(object):
         }, **kwargs.get('sanitizers', {}))
         self._logger = logging.getLogger(__name__)
         self.pagination_get_pages = pagination_get_pages
+        self.pagination_date_range = pagination_date_range
 
     def process_response(self, endpoint_key, response):
         validator = self.validators.get(endpoint_key, self.validators['default'])
         sanitizer = self.sanitizers.get(endpoint_key, self.sanitizers['default'])
         return sanitizer(validator(response))
+
+    def from_api_timestamp(self, api_ts, as_tz='UTC'):
+        as_tz = pytz.timezone(as_tz)
+        api_dt = datetime.fromtimestamp(api_ts)
+        return self.api_tz.localize(api_dt).astimezone(as_tz)
+
+    def to_api_timestamp(self, dt, dt_tz='UTC'):
+        dt_tz = pytz.timezone(dt_tz)
+        api_dt = dt_tz.localize(dt).astimezone(self.api_tz)
+        return int(api_dt.replace(tzinfo=None).timestamp())
