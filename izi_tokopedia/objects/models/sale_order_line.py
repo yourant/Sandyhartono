9735c2346baff8f5149a4e3e4747b913d9ecfe7f
Gusti Tammam <gustitammam@gmail.com>
2021-10-11 15:12:42 +0700

    [ADD][izi_mp,izi_tp] Initialize Get Order Line

--- izi_tokopedia/objects/models/sale_order_line.py
+++ izi_tokopedia/objects/models/sale_order_line.py
@@ -2,6 +2,7 @@
 # Copyright 2021 IZI PT Solusi Usaha Mudah
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SaleOrderLine(models.Model):
@@ -10,14 +11,22 @@ class SaleOrderLine(models.Model):
     tp_order_detail_id = fields.Char(string="Tokopedia Order Detail ID", required_if_marketplace="tokopedia")
 
     @classmethod
+    def _add_rec_mp_external_id(cls, mp_external_id_fields=None):
+        if not mp_external_id_fields:
+            mp_external_id_fields = []
+
+        mp_external_id_fields.append(('tokopedia', 'tp_order_detail_id'))
+        super(SaleOrderLine, cls)._add_rec_mp_external_id(mp_external_id_fields)
+
+    @classmethod
     def _add_rec_mp_field_mapping(cls, mp_field_mappings=None):
         if not mp_field_mappings:
             mp_field_mappings = []
 
         marketplace = 'tokopedia'
         mp_field_mapping = {
+            'order_id': ('order_id', None),
             'tp_order_detail_id': ('order_detail_id', lambda env, r: str(r)),
-            'tp_product_id': ('product_id', lambda env, r: str(r)),
             'name': ('product_name', None),
             'price_unit': ('product_price', None),
             'tp_subtotal_price': ('subtotal_price', None),
@@ -31,5 +40,33 @@ class SaleOrderLine(models.Model):
             'tp_sku': ('sku', None)
         }
 
+        def _handle_product_id(env, data):
+            product_obj = env['product.product']
+            mp_product_obj = env['mp.product']
+            mp_product_variant_obj = env['mp.product.variant']
+
+            product = product_obj
+            mp_product = mp_product_obj.search_mp_records('tokopedia', data)
+            mp_product_variant = mp_product_variant_obj.search_mp_records('tokopedia', data)
+
+            if mp_product.exists():
+                product = mp_product.get_product()
+            if mp_product_variant.exists():
+                product = mp_product_variant.get_product()
+
+            return product.id
+
+        mp_field_mapping.update({'product_id': ('product_id', _handle_product_id)})
+
         mp_field_mappings.append((marketplace, mp_field_mapping))
         super(SaleOrderLine, cls)._add_rec_mp_field_mapping(mp_field_mappings)
+
+    @api.model
+    def _finish_mapping_raw_data(self, sanitized_data, values):
+        sanitized_data, values = super(SaleOrderLine, self)._finish_mapping_raw_data(sanitized_data, values)
+
+        if not values.get('product_id'):
+            err_msg = 'Could not find matched record for MP Product "%s", please make sure this MP Product is mapped!'
+            raise ValidationError(err_msg % values['name'])
+
+        return sanitized_data, values
