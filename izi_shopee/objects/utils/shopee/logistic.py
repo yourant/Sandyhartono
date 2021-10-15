3f33fa5bb0243e6977b020330b608a0f6cba913c
rheynoapria <rheynoapria11@gmail.com>
2021-10-15 10:28:56 +0700

    [FIX][izi_shopee] build request depedency

--- izi_shopee/objects/utils/shopee/logistic.py
+++ izi_shopee/objects/utils/shopee/logistic.py
@@ -7,9 +7,9 @@ from .api import ShopeeAPI
 class ShopeeLogistic(ShopeeAPI):
 
     def get_logsitic_list(self):
-        prepared_request = self.endpoints.build_request('logistic_list',
-                                                        self.sp_account.partner_id,
-                                                        self.sp_account.partner_key,
-                                                        self.sp_account.shop_id,
-                                                        self.sp_account.access_token)
+        prepared_request = self.build_request('logistic_list',
+                                              self.sp_account.partner_id,
+                                              self.sp_account.partner_key,
+                                              self.sp_account.shop_id,
+                                              self.sp_account.access_token)
         return self.process_response('logistic_list', self.request(**prepared_request))
