46db17ed65e0c50a654eaa4074a27b1e5eb1ef82
rheynoapria <rheynoapria11@gmail.com>
2021-10-15 10:28:56 +0700

    [FIX][izi_shopee] build request depedency

--- izi_shopee/objects/utils/shopee/shop.py
+++ izi_shopee/objects/utils/shopee/shop.py
@@ -8,22 +8,22 @@ from .api import ShopeeAPI
 class ShopeeShop(ShopeeAPI):
 
     def get_profile_info(self, data):
-        prepared_request = self.endpoints.build_request('profile_info',
-                                                        self.sp_account.partner_id,
-                                                        self.sp_account.partner_key,
-                                                        self.sp_account.shop_id,
-                                                        self.sp_account.access_token)
+        prepared_request = self.build_request('profile_info',
+                                              self.sp_account.partner_id,
+                                              self.sp_account.partner_key,
+                                              self.sp_account.shop_id,
+                                              self.sp_account.access_token)
 
         sp_data_profile = self.process_response('profile_info', self.request(**prepared_request))
         data.update(sp_data_profile)
         return data
 
     def get_shop_info(self):
-        prepared_request = self.endpoints.build_request('shop_info',
-                                                        self.sp_account.partner_id,
-                                                        self.sp_account.partner_key,
-                                                        self.sp_account.shop_id,
-                                                        self.sp_account.access_token)
+        prepared_request = self.build_request('shop_info',
+                                              self.sp_account.partner_id,
+                                              self.sp_account.partner_key,
+                                              self.sp_account.shop_id,
+                                              self.sp_account.access_token)
 
         raw_data, sp_data_shop = self.process_response('shop_info', self.request(**prepared_request))
         raw_data.update({
