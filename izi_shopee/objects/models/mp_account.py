6bb8046042f4608e10d31e9a4076f60aa7313998
rheynoapria <rheynoapria11@gmail.com>
2021-10-22 19:02:51 +0700

    [IMP][izi_shopee] configurable unpaid,cancel order

--- izi_shopee/objects/models/mp_account.py
+++ izi_shopee/objects/models/mp_account.py
@@ -24,6 +24,16 @@ class MarketplaceAccount(models.Model):
     sp_partner_id = fields.Char(string="Partner ID", required_if_marketplace="shopee", states=READONLY_STATES)
     sp_partner_key = fields.Char(string="Partner Key", required_if_marketplace="shopee", states=READONLY_STATES)
     sp_shop_id = fields.Many2one(comodel_name="mp.shopee.shop", string="Current Shop")
+    sp_coins_product_id = fields.Many2one(comodel_name="product.product",
+                                          string="Default Shopee Coins Product",
+                                          default=lambda self: self._get_default_sp_coins_product_id())
+
+    @api.model
+    def _get_default_sp_coins_product_id(self):
+        sp_coins_product_tmpl = self.env.ref('izi_shopee.product_tmpl_shopee_coins', raise_if_not_found=False)
+        if sp_coins_product_tmpl:
+            return sp_coins_product_tmpl.product_variant_id.id
+        return False
 
     @api.model
     def shopee_get_account(self, **kwargs):
@@ -219,54 +229,94 @@ class MarketplaceAccount(models.Model):
     @api.multi
     @mp.shopee.capture_error
     def shopee_get_sale_order(self, time_range=False, **kwargs):
-        sale_order_obj = self.env['sale.order']
         mp_account_ctx = self.generate_context()
         if kwargs.get('force_update'):
             mp_account_ctx.update({'force_update': kwargs.get('force_update')})
+        order_obj = self.env['sale.order'].with_context(dict(mp_account_ctx, **self._context.copy()))
         _notify = self.env['mp.base']._notify
+        _logger = self.env['mp.base']._logger
         account_params = {}
         order_params = {}
         if self.mp_token_id.state == 'valid':
             account_params = {'access_token': self.mp_token_id.name}
         sp_account = self.shopee_get_account(**account_params)
-        sp_order_v2 = ShopeeOrder(sp_account, sanitizers=sale_order_obj.get_sanitizers(self.marketplace))
+        sp_order_v2 = ShopeeOrder(sp_account, sanitizers=order_obj.get_sanitizers(self.marketplace))
         sp_order_v1 = ShopeeOrder(sp_account, api_version="v1")
         _notify('info', 'Importing order from {} is started... Please wait!'.format(self.marketplace.upper()),
                 notif_sticky=True)
+
+        skipped = 0
+        force_update_ids = []
+
+        def get_order_income(sp_data_raws):
+            sp_order_raws, sp_order_sanitizeds = [], []
+            for index, data in enumerate(sp_data_raws):
+                sp_order_invoice = data.get('order_sn')
+                notif_msg = "(%s/%d) Getting order detail of %s... Please wait!" % (
+                    str(index + 1), len(sp_data_raws), sp_order_invoice
+                )
+                _logger(self.marketplace, notif_msg, notify=True, notif_sticky=True)
+                # get_income
+                income_data = sp_order_v1.get_income(**{'order_sn': data['order_sn']})
+                data.update({'order_income': income_data.get('order_income', False)})
+                sp_order_data_raw, sp_order_data_sanitized = order_obj.with_context(
+                    mp_account_ctx)._prepare_mapping_raw_data(raw_data=data)
+                sp_order_raws.append(sp_order_data_raw)
+                sp_order_sanitizeds.append(sp_order_data_sanitized)
+            return sp_order_raws, sp_order_sanitizeds
+
         if kwargs.get('params') == 'by_date_range':
+            sp_orders_by_mpexid = {}
+            sp_orders = order_obj.search([('mp_account_id', '=', self.id)])
+            for sp_order in sp_orders:
+                sp_orders_by_mpexid[sp_order.mp_external_id] = sp_order
+
             order_params.update({
                 'from_date': kwargs.get('from_date'),
                 'to_date': kwargs.get('to_date'),
                 'limit': mp_account_ctx.get('order_limit'),
-                'time_range': time_range
+                'time_range': time_range,
             })
-            sp_data_raw = sp_order_v2.get_order_list(**order_params)
+            sp_order_list = sp_order_v2.get_order_list(**order_params)
+            order_list = []
+            for sp_data_order in sp_order_list:
+                sp_order_invoice = sp_data_order.get('order_sn')
+                if sp_order_invoice in sp_orders_by_mpexid:
+                    existing_order = sp_orders_by_mpexid[sp_order_invoice]
+                else:
+                    existing_order = False
+                no_existing_order = not existing_order
+                if sp_data_order['order_status'] == 'CANCELLED' and no_existing_order:
+                    if not self.get_cancelled_orders:
+                        continue
+                if existing_order and mp_account_ctx.get('force_update'):
+                    force_update_ids.append(existing_order.id)
+                if not self.get_unpaid_orders:
+                    if sp_data_order['order_status'] != 'UNPAID':
+                        order_list.append({'order_sn': sp_order_invoice})
+                else:
+                    order_list.append({'order_sn': sp_order_invoice})
+
+            if order_list:
+                sp_data_raws = sp_order_v2.get_order_detail(sp_data=order_list)
+                sp_order_raws, sp_order_sanitizeds = get_order_income(sp_data_raws)
+
         elif kwargs.get('params') == 'by_mp_invoice_number':
+            sp_order_list = 1
             order_params.update({
                 'order_id': kwargs.get('mp_invoice_number')
             })
-            sp_data_raw = sp_order_v2.get_order_detail(**order_params)
+            sp_data_raws = sp_order_v2.get_order_detail(**order_params)
+            sp_order_raws, sp_order_sanitizeds = get_order_income(sp_data_raws)
 
-        sp_order_raws, sp_order_sanitizeds = [], []
-        for data in sp_data_raw:
-            # get awb url
-            # if data['shipping_document_info']:
-            #     if data['shipping_document_info']['tracking_number']:
-            #         awb_data = sp_order_v1.get_airways_bill(**{'order_sn': data['order_sn']})
-            #         data['awb_url'] = awb_data.get(data['order_sn'], False)
-            #     else:
-            #         data['awb_url'] = False
-            # else:
-            #     data['awb_url'] = False
+        _logger(self.marketplace, 'Processed %s order(s) from %s of total orders imported!' % (
+                len(sp_data_raws), len(sp_order_list)
+                ), notify=True, notif_sticky=True)
 
-            # get_income
-            income_data = sp_order_v1.get_income(**{'order_sn': data['order_sn']})
-            data.update({'order_income': income_data.get('order_income', False)})
-
-            sp_order_data_raw, sp_order_data_sanitized = sale_order_obj.with_context(
-                mp_account_ctx)._prepare_mapping_raw_data(raw_data=data)
-            sp_order_raws.append(sp_order_data_raw)
-            sp_order_sanitizeds.append(sp_order_data_sanitized)
+        if force_update_ids:
+            order_obj = order_obj.with_context(dict(order_obj._context.copy(), **{
+                'force_update_ids': force_update_ids
+            }))
 
         check_existing_records_params = {
             'identifier_field': 'sp_order_id',
@@ -274,15 +324,9 @@ class MarketplaceAccount(models.Model):
             'mp_data': sp_order_sanitizeds,
             'multi': isinstance(sp_order_sanitizeds, list)
         }
-        check_existing_records = sale_order_obj.with_context(mp_account_ctx).check_existing_records(
+        check_existing_records = order_obj.with_context(mp_account_ctx).check_existing_records(
             **check_existing_records_params)
-
-        # NEED EVALUATE
-        # if time_range == 'create_time':
-        # check_existing_records.pop('need_update_records')
-        # elif time_range == 'update_time':
-        # check_existing_records.pop('need_create_records')
-        sale_order_obj.with_context(mp_account_ctx).handle_result_check_existing_records(check_existing_records)
+        order_obj.with_context(mp_account_ctx).handle_result_check_existing_records(check_existing_records)
 
     @ api.multi
     def shopee_get_orders(self, **kwargs):
