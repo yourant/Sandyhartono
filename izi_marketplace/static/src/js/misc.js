e4c0c61d78ca114c2a6fa0e3c8a89d4a3d6f8d98
Gusti Tammam <gustitammam@gmail.com>
2021-10-23 10:22:50 +0700

    [IMP][izi_mp] Using AbstractAction as Base of Client Action

--- izi_marketplace/static/src/js/misc.js
+++ izi_marketplace/static/src/js/misc.js
@@ -3,65 +3,82 @@ odoo.define('izi_marketplace.framework', function (require) {
 
   let core = require('web.core');
   let framework = require('web.framework');
+  let AbstractAction = require('web.AbstractAction');
+  let ControlPanelMixin = require('web.ControlPanelMixin');
   let originblockUI = framework.blockUI;
   let originUnblockUI = framework.unblockUI;
 
-  function closeNotifications(params) {
-    if (!params) {
-      params = {}
-    }
-    let $notifs = $('.o_notification .o_close');
-    if ($notifs.length > 0) {
-      if (!params.force_show_number || params.close_on_finish) {
-        setTimeout(function () {
-          let $notif = [].shift.call($notifs);
-          if ($notif) {
-            $notif.click();
-          }
-          closeNotifications(params);
-        }, 500);
-      } else if ($notifs.length > params.force_show_number) {
-        setTimeout(function () {
-          let $notifs_to_close = $notifs.slice(0, $notifs.length - params.force_show_number);
-          if ($notifs_to_close) {
-            $notifs_to_close.click();
-          }
-          closeNotifications(params);
-        }, 500);
-      } else if ($notifs.length <= params.force_show_number) {
-        setTimeout(function () {
-          if (!params.hasOwnProperty('close_on_finish')) {
-            params.close_on_finish = true;
-          }
-          closeNotifications(params);
-        }, 500);
-      } else {
-        setTimeout(function () {
-          closeNotifications(params);
-        }, 500);
+  let CloseNotificationAction = AbstractAction.extend(ControlPanelMixin, {
+    init: function (parent, action) {
+      this.actionManager = parent;
+      this.action = action;
+      this.originalUrl = window.location.href;
+      return this._super.apply(this, arguments);
+    },
+    start: function () {
+      let self = this;
+      return this._super.apply(this, arguments).then(function () {
+        let actionManager = self.actionManager;
+        let action = self.action;
+        // let controller = parent.getCurrentController()
+        let params = action.params || {};
+        self.closeNotifications(params);
+        if (action.context.close_notifications_and_wizard) {
+          actionManager.do_action({'type': 'ir.actions.act_window_close'});
+        }
+      });
+    },
+    closeNotifications: function (params) {
+      if (window.location.href !== this.originalUrl) {
+        window.location = this.originalUrl;
+      }
+      let self = this;
+      if (!params) {
+        params = {}
+      }
+      let $notifs = $('.o_notification .o_close');
+      if ($notifs.length > 0) {
+        if (!params.force_show_number || params.close_on_finish) {
+          setTimeout(function () {
+            let $notif = [].shift.call($notifs);
+            if ($notif) {
+              $notif.click();
+            }
+            self.closeNotifications(params);
+          }, 500);
+        } else if ($notifs.length > params.force_show_number) {
+          setTimeout(function () {
+            let $notifs_to_close = $notifs.slice(0, $notifs.length - params.force_show_number);
+            if ($notifs_to_close) {
+              $notifs_to_close.click();
+            }
+            self.closeNotifications(params);
+          }, 500);
+        } else if ($notifs.length <= params.force_show_number) {
+          setTimeout(function () {
+            if (!params.hasOwnProperty('close_on_finish')) {
+              params.close_on_finish = true;
+            }
+            self.closeNotifications(params);
+          }, 500);
+        } else {
+          setTimeout(function () {
+            self.closeNotifications(params);
+          }, 500);
+        }
       }
     }
-  }
+  });
 
   framework.blockUI = function () {
-    closeNotifications({force_show_number: 1, close_on_finish: false});
+    new CloseNotificationAction().closeNotifications({force_show_number: 1, close_on_finish: false});
     return originblockUI();
   }
 
   framework.unblockUI = function () {
-    closeNotifications();
+    new CloseNotificationAction().closeNotifications();
     return originUnblockUI();
   }
 
-  function closeNotificationAction(parent, action) {
-    let controller = parent.inner_widget.active_view.controller
-    let params = action.params || {};
-    closeNotifications(params);
-    if (action.context.close_notifications_and_wizard) {
-      parent.do_action({'type': 'ir.actions.act_window_close'});
-    }
-    controller.reload();
-  }
-
-  core.action_registry.add('close_notifications', closeNotificationAction);
+  core.action_registry.add('close_notifications', CloseNotificationAction);
 });
