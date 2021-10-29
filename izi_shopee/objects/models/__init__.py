a7496e4ab9902c539cb3ff82efd511e80917f399
rheynoapria <rheynoapria11@gmail.com>
2021-10-29 17:30:10 +0700

    [ADD][izi_mp,izi_sp] action request pickup

--- izi_shopee/objects/models/__init__.py
+++ izi_shopee/objects/models/__init__.py
@@ -5,8 +5,10 @@ from . import mp_account
 from . import mp_token
 from . import mp_shopee_logistic
 from . import mp_shopee_shop
+from . import mp_shopee_shop_address
 from . import mp_product
 from . import mp_product_image
 from . import mp_product_variant
 from . import sale_order
 from . import sale_order_line
+from . import mp_shopee_order_pickup_info
---
+++ izi_shopee/objects/wizard/wiz_sp_order_pickup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 IZI PT Solusi Usaha Mudah
+import time
+
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
+from odoo.addons.izi_marketplace.objects.utils.tools import mp
+from odoo.addons.izi_shopee.objects.utils.shopee.order import ShopeeOrder
+
+
+class WizardShopeeOrderPickup(models.TransientModel):
+    _name = 'wiz.sp_order_pickup'
+    _description = 'Shopee Order Pickup Wizard'
+
+    order_ids = fields.Many2many(comodel_name="sale.order", relation="rel_sp_order_pickup_sale_order",
+                                 column1="order_pickup_id", column2="order_id", string="Order(s)", required=True)
+    pickup_id = fields.Many2one(comodel_name='mp.shopee.order.pickup.info', string='Pickup Time')
+    address_id = fields.Many2one(comodel_name='mp.shopee.shop.address', string='Pickup Address')
+
+    @ mp.shopee.capture_error
+    def confirm(self):
+        for order in self.order_ids:
+            if order.mp_account_id.mp_token_id.state == 'valid':
+                params = {'access_token': order.mp_account_id.mp_token_id.name}
+                sp_account = order.mp_account_id.shopee_get_account(**params)
+                sp_order = ShopeeOrder(sp_account)
+
+                kwargs = {
+                    'order_sn': order.mp_external_id,
+                    'pickup': {
+                        "tracking_no": "",
+                        "address_id": int(self.address_id.mp_external_id),
+                        "pickup_time_id": self.pickup_id.mp_external_id
+                    }
+                }
+                mp_shopee_order_pickup_info_obj = self.env['mp.shopee.order.pickup.info']
+                pickup_ids = mp_shopee_order_pickup_info_obj.search(
+                    [('order_id', '=', order.id), ('pickup_id', '!=', self.pickup_id.id)])
+                for pickup in pickup_ids:
+                    pickup.active = False
+                action_status = sp_order.action_ship_order(**kwargs)
+                if action_status == "success":
+                    order.action_confirm()
+                    time.sleep(3)
+                    order.shopee_fetch_order()
