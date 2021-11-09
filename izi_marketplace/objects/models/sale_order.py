586edbcacacfda782a7f6a0d80e3552db322a9f4
rheynoapria <39023984+rheynoapria@users.noreply.github.com>
2021-11-09 15:26:24 +0700

    [ADD][izi_mp] action multi get book code/awb num

--- izi_marketplace/objects/models/sale_order.py
+++ izi_marketplace/objects/models/sale_order.py
@@ -276,3 +276,18 @@ class SaleOrder(models.Model):
                 raise ValidationError('Please select the same marketplace account.')
         else:
             raise ValidationError('Please select the same marketplace channel.')
+
+    @api.multi
+    def get_awb_num(self):
+        marketplace = self.mapped('marketplace')
+        mp_account_ids = self.mapped('mp_account_id.id')
+        if marketplace.count(marketplace[0]) == len(marketplace):
+            if mp_account_ids.count(mp_account_ids[0]) == len(mp_account_ids):
+                if hasattr(self, '%s_get_booking_code' % marketplace[0]):
+                    return getattr(self, '%s_get_booking_code' % marketplace[0])()
+                elif hasattr(self, '%s_get_awb_num' % marketplace[0]):
+                    return getattr(self, '%s_get_awb_num' % marketplace[0])()
+            else:
+                raise ValidationError('Please select the same marketplace account.')
+        else:
+            raise ValidationError('Please select the same marketplace channel.')
