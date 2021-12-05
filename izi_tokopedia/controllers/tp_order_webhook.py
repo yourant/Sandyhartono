318b1f98da3a3db6778a8dcafd44f4c94d77782b
rheynoapria <rheynoapria11@gmail.com>
2021-12-05 12:51:54 +0700

    [ADD][izi_tp] create order to model webhook order

--- izi_tokopedia/controllers/tp_order_webhook.py
+++ izi_tokopedia/controllers/tp_order_webhook.py
@@ -6,6 +6,8 @@ from odoo.http import *
 import base64
 import requests
 import logging
+from odoo.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT
+
 _logger = logging.getLogger(__name__)
 
 
@@ -20,6 +22,18 @@ class IZITokopedia(http.Controller):
                 mp_account = request.env['mp.account'].sudo().search([('tp_fs_id', '=', fs_id)])
                 if mp_account.mp_webhook_state == 'registered':
                     marketplace = mp_account.marketplace
+                    mp_webhook_order_obj = request.env['mp.webhook.order'].sudo()
+                    mp_webhook_order_obj.create({
+                        'mp_invoice_number': json_body.get('invoice_num,', ''),
+                        'tp_order_id': json_body.get('order_id,', False),
+                        'mp_account_id': mp_account.id,
+                        'order_update_time': datetime.fromtimestamp(
+                            time.mktime(time.gmtime(json_body.get('create_time'))))
+                        .strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+                        'tp_order_status': json_body.get('order_status')
+                    })
+                    _logger.info('Success Create Tokopedia Order: %s with status %s' %
+                                 (json_body.get('invoice_num'), json_body.get('order_status')))
                     kwargs = {'params': 'by_mp_invoice_number',
                               'mp_order_id': json_body.get('order_id'),
                               'force_update': mp_account._context.get('force_update', False)}
@@ -48,15 +62,19 @@ class IZITokopedia(http.Controller):
         if request._request_type == 'json':
             json_body = request.jsonrequest
             fs_id = json_body.get('fs_id', False)
-            if fs_id:
-                mp_account = request.env['mp.account'].sudo().search([('tp_fs_id', '=', fs_id)])
-                if mp_account.mp_webhook_state == 'registered':
-                    marketplace = mp_account.marketplace
-                    kwargs = {'params': 'by_mp_invoice_number',
-                              'mp_order_id': json_body.get('order_id'),
-                              'force_update': mp_account._context.get('force_update', False),
-                              'skip_create': True}
-                    if hasattr(mp_account, '%s_get_orders' % marketplace):
-                        getattr(mp_account, '%s_get_orders' % marketplace)(**kwargs)
+            order_status = str(json_body.get('order_status'))
+            if order_status not in ['220', '221', '11', '100', '103', '200']:
+                if fs_id:
+                    _logger.info('New Order Status Change From Tokopedia: %s with status %s' %
+                                 (json_body.get('order_id,', ''), json_body.get('order_status')))
+                    mp_account = request.env['mp.account'].sudo().search([('tp_fs_id', '=', fs_id)])
+                    if mp_account.mp_webhook_state == 'registered':
+                        marketplace = mp_account.marketplace
+                        kwargs = {'params': 'by_mp_invoice_number',
+                                  'mp_order_id': json_body.get('order_id'),
+                                  'force_update': mp_account._context.get('force_update', False),
+                                  'skip_create': True}
+                        if hasattr(mp_account, '%s_get_orders' % marketplace):
+                            getattr(mp_account, '%s_get_orders' % marketplace)(**kwargs)
         res = Response('Success', status=200)
         return res
