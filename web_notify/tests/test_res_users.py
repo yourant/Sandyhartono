e273b3dccc4bbadbc3c1f6792771a4a34ad08725
rheynoapria <rheynoapria11@gmail.com>
2021-12-09 17:29:50 +0700

    [ADD][web_notify] web notify version 13.0

--- web_notify/tests/test_res_users.py
+++ web_notify/tests/test_res_users.py
@@ -2,23 +2,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import json
+from unittest import mock
 
-import mock
 from odoo import exceptions
-from odoo.addons.bus.models.bus import json_dump
-from ..models.res_users import SUCCESS, DANGER, WARNING, INFO, DEFAULT
 from odoo.tests import common
+
+from odoo.addons.bus.models.bus import json_dump
+
+from ..models.res_users import DANGER, DEFAULT, INFO, SUCCESS, WARNING
 
 
 class TestResUsers(common.TransactionCase):
     def test_notify_success(self):
         bus_bus = self.env["bus.bus"]
         domain = [
-            (
-                "channel",
-                "=",
-                json_dump(self.env.user.notify_success_channel_name),
-            )
+            ("channel", "=", json_dump(self.env.user.notify_success_channel_name))
         ]
         existing = bus_bus.search(domain)
         test_msg = {"message": "message", "title": "title", "sticky": True}
@@ -30,13 +28,7 @@ class TestResUsers(common.TransactionCase):
 
     def test_notify_danger(self):
         bus_bus = self.env["bus.bus"]
-        domain = [
-            (
-                "channel",
-                "=",
-                json_dump(self.env.user.notify_danger_channel_name),
-            )
-        ]
+        domain = [("channel", "=", json_dump(self.env.user.notify_danger_channel_name))]
         existing = bus_bus.search(domain)
         test_msg = {"message": "message", "title": "title", "sticky": True}
         self.env.user.notify_danger(**test_msg)
@@ -48,11 +40,7 @@ class TestResUsers(common.TransactionCase):
     def test_notify_warning(self):
         bus_bus = self.env["bus.bus"]
         domain = [
-            (
-                "channel",
-                "=",
-                json_dump(self.env.user.notify_warning_channel_name),
-            )
+            ("channel", "=", json_dump(self.env.user.notify_warning_channel_name))
         ]
         existing = bus_bus.search(domain)
         test_msg = {"message": "message", "title": "title", "sticky": True}
@@ -64,9 +52,7 @@ class TestResUsers(common.TransactionCase):
 
     def test_notify_info(self):
         bus_bus = self.env["bus.bus"]
-        domain = [
-            ("channel", "=", json_dump(self.env.user.notify_info_channel_name))
-        ]
+        domain = [("channel", "=", json_dump(self.env.user.notify_info_channel_name))]
         existing = bus_bus.search(domain)
         test_msg = {"message": "message", "title": "title", "sticky": True}
         self.env.user.notify_info(**test_msg)
@@ -78,11 +64,7 @@ class TestResUsers(common.TransactionCase):
     def test_notify_default(self):
         bus_bus = self.env["bus.bus"]
         domain = [
-            (
-                "channel",
-                "=",
-                json_dump(self.env.user.notify_default_channel_name),
-            )
+            ("channel", "=", json_dump(self.env.user.notify_default_channel_name))
         ]
         existing = bus_bus.search(domain)
         test_msg = {"message": "message", "title": "title", "sticky": True}
@@ -95,9 +77,7 @@ class TestResUsers(common.TransactionCase):
     def test_notify_many(self):
         # check that the notification of a list of users is done with
         # a single call to the bus
-        with mock.patch(
-            "odoo.addons.bus.models.bus.ImBus.sendmany"
-        ) as mockedSendMany:
+        with mock.patch("odoo.addons.bus.models.bus.ImBus.sendmany") as mockedSendMany:
             users = self.env.user.search([(1, "=", 1)])
             self.assertTrue(len(users) > 1)
             users.notify_warning(message="message")
@@ -116,7 +96,7 @@ class TestResUsers(common.TransactionCase):
 
     def test_notify_other_user(self):
         other_user = self.env.ref("base.user_demo")
-        other_user_model = self.env["res.users"].sudo(other_user)
+        other_user_model = self.env["res.users"].with_user(other_user)
         with self.assertRaises(exceptions.UserError):
             other_user_model.browse(self.env.uid).notify_info(message="hello")
 
